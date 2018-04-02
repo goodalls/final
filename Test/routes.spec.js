d256f7e92850145d878e0dd1076749e9e8b31738
Jeff Goodall <goodallhj@gmail.com>
2018-04-02 09:22:16 -0600

    initial commit, adding testing features

---
+++ Test/routes.spec.js
@@ -0,0 +1,15 @@
+const chai = require('chai');
+const should = chai.should();
+const expect = chai.expect;
+const chaiHttp = require('chai-http');
+const server = require('../server');
+
+chai.use(chaiHttp);
+
+describe('Client Routes', () => {
+
+});
+
+describe('API Routes', () => {
+
+});

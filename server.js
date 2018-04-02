6d9a345645d248779c870d282b5f72cb3d4814b5
Jeff Goodall <goodallhj@gmail.com>
2018-04-02 10:22:52 -0600

    first GET

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 
 const environment = process.env.NODE_ENV || 'development';
-const configuration = require('../knexfile')[environment];
+const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 
 app.set('port', process.env.PORT || 3000);
@@ -11,8 +11,21 @@ app.locals.title = 'Final';
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
-app.get('/', (request, response) => {
-  
+app.get('/', (request, response) => {});
+
+app.get('/api/v1/items', (request, response) => {
+  database('list')
+    .select()
+    .then(items => {
+      response.status(200).json(items);
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+});
+
+app.use((request, response) => {
+  response.status(404).send('Sorry can\'t find that!');
 });
 
 app.listen(app.get('port'), () => {

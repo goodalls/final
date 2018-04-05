d33154c61c6865f228eb4f766c48e53320e303b1
Jeff Goodall <goodallhj@gmail.com>
2018-04-05 16:18:37 -0600

    Adding Card Styling

--- public/script.js
+++ public/script.js
@@ -27,16 +27,16 @@ const addToList = id => {
   fetch(`/api/v1/items/${id.id}`)
     .then(response => response.json())
     .then(item => {
-      console.log(item[0]);
-      let {id, name, packed} = item[0]
-      let content = (`
+      let { id, name, packed } = item[0];
+      const klass = packed == 'true' ? 'checked' : '';
+      let content = `
       <article id="${id}" class="card">
       <h4>${name}</h4>
       <label for="packed">Packed</label> 
-      <input type="checkbox" value="${packed}" id="packed">
+      <input type="checkbox" value="${packed}" id="packed" ${klass}>
       <button id="delete">Delete</button>
       </article>
-    `)
+    `;
       $('#packing-list').prepend(content);
     });
 };
@@ -45,44 +45,44 @@ window.onload = () => {
   fetch('/api/v1/items/')
     .then(response => response.json())
     .then(items => {
-      items.forEach((item) => {
-        let {id, name, packed} = item
+      items.forEach(item => {
+        let { id, name, packed } = item;
         console.log(packed);
-        const klass = packed == 'true'?'checked':'';
-        let content = (`
+        const klass = packed == 'true' ? 'checked' : '';
+        let content = `
         <article id="${id}" class="card">
         <h4>${name}</h4>
         <label for="packed">Packed</label> 
         <input type="checkbox" value="${packed}" id="packed" ${klass}>
         <button id="delete">Delete</button>
         </article>
-      `)
+      `;
         $('#packing-list').prepend(content);
-      })
-    }); 
-}
+      });
+    });
+};
 
-$('#packing-list').click((event) => {
-  const itemID = event.target.closest('article').id
+$('#packing-list').click(event => {
+  const itemID = event.target.closest('article').id;
   if (event.target.id === 'delete') {
-    fetch(`/api/v1/items/${itemID}` , {
+    fetch(`/api/v1/items/${itemID}`, {
       method: 'DELETE',
       headers: {
         'content-type': 'application/json'
       }
     })
-    .then(response => response.json())
-    .then(remove => {
-      event.target.closest('article').remove();
-    })
-    .catch(err => {
-      throw err;
-    })
+      .then(response => response.json())
+      .then(remove => {
+        event.target.closest('article').remove();
+      })
+      .catch(err => {
+        throw err;
+      });
   }
 
   if (event.target.id === 'packed') {
-    const packedValue = event.target.value === 'false'? true:false
-    fetch(`/api/v1/items/${itemID}` , {
+    const packedValue = event.target.value === 'false' ? true : false;
+    fetch(`/api/v1/items/${itemID}`, {
       method: 'PATCH',
       headers: {
         'content-type': 'application/json'
@@ -93,11 +93,11 @@ $('#packing-list').click((event) => {
     })
       .then(response => response.json())
       .then(update => {
-        packed = packedValue
+        packed = packedValue;
         //update packed to packedValue
       })
       .catch(err => {
         throw err;
-      })
-    }
-  });
+      });
+  }
+});
